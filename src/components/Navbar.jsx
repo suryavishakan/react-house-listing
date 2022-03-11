b61ec82def7b769f529548a6b431e14f3cfd530b
surya vishakan <suryavishakan050@gmail.com>
2022-03-11 21:59:04 +0530

    modified navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { useNavigate, useLocation } from "react-router-dom";
+// icons
 import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as PersonOutline } from "../assets/svg/personOutlineIcon.svg";
 const Navbar = () => {
+  // initialize the hooks
   const navigate = useNavigate();
   const location = useLocation();
+
+  // if the route path matches, then return true
   const pathMatchRoute = (route) => {
     if (route === location.pathname) {
       return true;
@@ -15,6 +19,7 @@ const Navbar = () => {
       <nav className="navbarNav">
         <ul className="navbarListItems">
           <li className="navbarListItem" onClick={() => navigate("/")}>
+            {/* apply active class to the icon if the route path matches */}
             <ExploreIcon
               fill={pathMatchRoute("/") ? "#2c2c2c" : "#8f8f8f"}
               width=" 34px"
