638d2d41cfc0e0e702ef45642f23c2b2dd02b42c
surya vishakan <suryavishakan050@gmail.com>
2022-03-16 21:56:50 +0530

    created house listing page

---
+++ src/pages/Category.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// firestore
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+// db
+import { db } from "../firebase/firebase.config";
+// react toastify
+import { toast } from "react-toastify";
+// spinner
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
+
+const Category = () => {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // get reference
+        const listingsRef = collection(db, "listings");
+        // query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          limit(10)
+        );
+        // execute query
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+        console.log(listings);
+      } catch (err) {
+        toast.error("Could not fetch listings");
+      }
+    };
+    fetchListings();
+  }, [params.categoryName]);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          Places for {params.categoryName === "rent" ? "rent" : "sale"}
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
+    </div>
+  );
+};
+
+export default Category;
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
+// images
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+        <main>
+          {/* slider */}
+          <p className="exploreCategoryHeading">Categories</p>
+          <div className="exploreCategories">
+            <Link to="/category/rent">
+              <img
+                src={rentCategoryImage}
+                alt="rent"
+                className="exploreCategoryImg"
+              />
+              <p className="exploreCategoryName">Places for rent</p>
+            </Link>
+            <Link to="/category/sale">
+              <img
+                src={sellCategoryImage}
+                alt="sell"
+                className="exploreCategoryImg"
+              />
+              <p className="exploreCategoryName">Places for sale</p>
+            </Link>
+          </div>
+        </main>
+      </header>
+    </div>
+  );
 };
 
 export default Explore;
