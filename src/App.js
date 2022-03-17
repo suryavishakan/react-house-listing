e560d87074adab3466ed1e84deb72eddaea32cf3
surya vishakan <suryavishakan050@gmail.com>
2022-03-17 19:54:13 +0530

    created single listing page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 // private route
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -37,6 +38,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
---
+++ src/pages/Listing.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+// firebase
+import { getAuth } from "firebase/auth";
+// firestore
+import { getDoc, doc } from "firebase/firestore";
+// db
+import { db } from "../firebase/firebase.config";
+//components
+import Spinner from "../components/Spinner";
+// icon
+import ShareIcon from "../assets/svg/shareIcon.svg";
+// react toastify
+import { toast } from "react-toastify";
+
+const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [sharedLinkCopied, setSharedLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      } else {
+        toast.error("couldn't fetch listing");
+      }
+    };
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <main>
+      <div className="shareIconDiv">
+        <img
+          src={ShareIcon}
+          alt="share icon"
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setSharedLinkCopied(true);
+            setTimeout(() => {
+              setSharedLinkCopied(false);
+            }, 1000);
+          }}
+        />
+      </div>
+      {sharedLinkCopied && <p className="linkCopied">Link Copied!</p>}
+
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            $ {listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathrooms"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+        {/* Map */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className="primaryButton"
+          >
+            Contact Owner
+          </Link>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Listing;
